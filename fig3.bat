806bf0e7c0a7eed178b1574a1bb5dd436759c643
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2015-10-28 09:45:51 +0200

    Update fig3.bat

--- fig3.bat
+++ fig3.bat
@@ -1,1 +1,1 @@
-@java -Xmx1024m -jar garch.jar -j 3 99 -gp 10 -rp 1000000 -op 30 -pdfllim -2 6 --useAbs -lim -1e6 1e6 -x0 0.3 -ot fig3. -pars 1e-6 1e-3 1 -nlin -eta 1.5 -seed 1416481288888
+@java -Xmx1024m -jar garch.jar -j 3 999 -gp 10 -rp 1000000 -op 30 -pdfllim -2 6 --useAbs -lim -1e6 1e6 -x0 0.3 -ot fig3. -pars 1e-6 1e-3 1 -nlin -eta 1.5 -seed 1416481288888
