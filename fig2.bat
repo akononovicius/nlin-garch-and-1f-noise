21eb8e22bbdbf156d310ecc69f2599bfe8cb78d8
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2015-10-28 09:43:13 +0200

    Update fig2.bat

--- fig2.bat
+++ fig2.bat
@@ -1,3 +1,3 @@
-@java -Xmx1024m -jar garch.jar -j 1 33 -rp 1000000 -op 300 -pdfllim -2 6 -lim -1e6 1e6 -x0 1 -ot fig2.0. -pars 0.015 0.1 0.89
-@java -Xmx1024m -jar garch.jar -j 1 33 -rp 1000000 -op 300 -pdfllim -2 6 -lim -1e6 1e6 -x0 1 -ot fig2.1. -pars 0.015 0.1 0.88
-@java -Xmx1024m -jar garch.jar -j 1 33 -rp 1000000 -op 300 -pdfllim -2 6 -lim -1e6 1e6 -x0 1 -ot fig2.2. -pars 0.015 0.1 0.87
+@java -Xmx1024m -jar garch.jar -j 3 99 -rp 1000000 -op 30 --useAbs -pdfllim -2 6 -lim -1e6 1e6 -x0 0.3 -ot 0. -pars 0.015 0.1 0.89 -seed 1416923589990
+@java -Xmx1024m -jar garch.jar -j 3 99 -rp 1000000 -op 30 --useAbs -pdfllim -2 6 -lim -1e6 1e6 -x0 0.3 -ot 1. -pars 0.015 0.1 0.88 -seed 1416923663296
+@java -Xmx1024m -jar garch.jar -j 3 99 -rp 1000000 -op 30 --useAbs -pdfllim -2 6 -lim -1e6 1e6 -x0 0.3 -ot 2. -pars 0.015 0.1 0.87 -seed 1416923735319
