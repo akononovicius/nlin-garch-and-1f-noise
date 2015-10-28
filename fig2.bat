b840f1b0b2589358897cdb80151c9a3a0311c618
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2015-10-28 09:10:21 +0200

    Create fig2.bat

---
+++ fig2.bat
@@ -0,0 +1,3 @@
+@java -Xmx1024m -jar garch.jar -j 1 33 -rp 1000000 -op 300 -pdfllim -2 6 -lim -1e6 1e6 -x0 1 -ot fig2.0. -pars 0.015 0.1 0.89
+@java -Xmx1024m -jar garch.jar -j 1 33 -rp 1000000 -op 300 -pdfllim -2 6 -lim -1e6 1e6 -x0 1 -ot fig2.1. -pars 0.015 0.1 0.88
+@java -Xmx1024m -jar garch.jar -j 1 33 -rp 1000000 -op 300 -pdfllim -2 6 -lim -1e6 1e6 -x0 1 -ot fig2.2. -pars 0.015 0.1 0.87
